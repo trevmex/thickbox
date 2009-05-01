ec308e83cb1492d7943f0e7ee4b8f1a46601c9c2
Trevor Lalish-Menagh <trev@trevreport.org>
2009-04-30 23:47:07 -0400

    fixed problem with helper file

--- install.rb
+++ install.rb
@@ -7,11 +7,11 @@ thickbox_js       = File.dirname(__FILE__) + '/../../../public/javascripts/thick
 thickbox_pack_js  = File.dirname(__FILE__) + '/../../../public/javascripts/thickbox.pack.js'
 thickbox_css      = File.dirname(__FILE__) + '/../../../public/stylesheets/thickbox.css'
 
-FileUtils.cp File.dirname(__FILE__) + '/public/images/loading.gif',           loading_gif   unless File.exist?(loading_gif)
-FileUtils.cp File.dirname(__FILE__) + '/public/javascripts/jquery.js',        lightbox_js   unless File.exist?(jquery_js)
-FileUtils.cp File.dirname(__FILE__) + '/public/javascripts/jquery.pack.js',   lightbox_js   unless File.exist?(jquery_pack_js)
-FileUtils.cp File.dirname(__FILE__) + '/public/javascripts/thickbox.js',      lightbox_js   unless File.exist?(thickbox_js)
-FileUtils.cp File.dirname(__FILE__) + '/public/javascripts/thickbox.pack.js', lightbox_js   unless File.exist?(thickbox_pack_js)
-FileUtils.cp File.dirname(__FILE__) + '/public/stylesheets/thickbox.css',     lightbox_css  unless File.exist?(thickbox_css)
+FileUtils.cp File.dirname(__FILE__) + '/public/images/loading.gif',           loading_gif       unless File.exist?(loading_gif)
+FileUtils.cp File.dirname(__FILE__) + '/public/javascripts/jquery.js',        jquery_js         unless File.exist?(jquery_js)
+FileUtils.cp File.dirname(__FILE__) + '/public/javascripts/jquery.pack.js',   jquery_pack_js    unless File.exist?(jquery_pack_js)
+FileUtils.cp File.dirname(__FILE__) + '/public/javascripts/thickbox.js',      thickbox_js       unless File.exist?(thickbox_js)
+FileUtils.cp File.dirname(__FILE__) + '/public/javascripts/thickbox.pack.js', thickbox_pack_js  unless File.exist?(thickbox_pack_js)
+FileUtils.cp File.dirname(__FILE__) + '/public/stylesheets/thickbox.css',     thickbox_css      unless File.exist?(thickbox_css)
 
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
